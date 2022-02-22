8a3cf9f8fe7ff795a82055fc1d8bac265dcb9a45
Simone <startae14@gmail.com>
2022-02-22 21:50:35 +0100

    define the launches model for the POST /launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,8 +1,10 @@
 const launches = new Map();
 
+let latestFlighNumber = 100;
+
 // example launch
 const launch = {
-  flightNumber: 100,
+  flightNumber: latestFlighNumber,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
@@ -18,6 +20,20 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlighNumber++;
+  launches.set(
+    latestFlighNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['Zero To Mastery', 'NASA'],
+      flightNumber: latestFlighNumber,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
