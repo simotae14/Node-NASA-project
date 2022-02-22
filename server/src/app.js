a92e98378164d51a45e4b0faf8e4f26ecca9907a
Simone <startae14@gmail.com>
2022-02-22 16:45:13 +0100

    Client Side Routing for Fullstack applications

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, '..','public')));
 app.use(planetsRouter);
 // add launchesRouter as a middleware
 app.use(launchesRouter);
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
