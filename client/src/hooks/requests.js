b261d7977312a7fc40b996728630ac3cdd3857ad
Simone <startae14@gmail.com>
2022-02-23 15:31:37 +0100

    implement the FE solution to call DELETE /launches/:id

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -34,15 +34,25 @@ async function httpSubmitLaunch(launch) {
       body: JSON.stringify(launch),
     });
   } catch (error) {
+    console.log(error);
     return {
       ok: false,
     };
   }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'DELETE',
+    });
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+    };
+  }
 }
 
 export {
