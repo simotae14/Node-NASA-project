160d312f2f11aba0caa1bc0484f141c699826257
Simone <startae14@gmail.com>
2022-02-23 14:59:35 +0100

    connect to the FE the POST /launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -10,7 +10,7 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
   // validation required fields
-  if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+  if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
     return res.status(400).json({
       error: 'Missing required launch property',
     });
