222a6f78138de63486463d1c4a536a86e9a15a70
Simone <startae14@gmail.com>
2022-03-07 12:16:57 +0100

    implement the mongoose way to delete a launch modifying the existsLaunchWithId and abortLaunchById functions to use the mongoose methods

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -17,8 +17,11 @@ const launch = {
 
 saveLaunch(launch);
 
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+  return await launches
+    .findOne({
+      flightNumber: launchId,
+    });
 }
 
 async function getLatestFlightNumber() {
@@ -67,11 +70,15 @@ async function scheduleNewLaunch(launch) {
   await saveLaunch(newLaunch);
 }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function abortLaunchById(launchId) {
+  const aborted = await launches.updateOne({
+    flightNumber: launchId,
+  }, {
+    upcoming: false,
+    success: false,
+  });
+  console.log('aborted', aborted)
+  return aborted.modifiedCount === 1;
 }
 
 module.exports = {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -30,21 +30,27 @@ async function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch);
 }
 
-function httpAbortLaunch(req, res) {
+async function httpAbortLaunch(req, res) {
   // get launch id
   const launchId = Number(req.params.id);
 
+  const existsLaunch = await existsLaunchWithId(launchId);
   // if launch doesn't exist
-  if (!existsLaunchWithId(launchId)) {
+  if (!existsLaunch) {
     return res.status(404).json({
       error: 'Launch not found',
     });
   }
 
-  const aborted = abortLaunchById(launchId);
+  const aborted = await abortLaunchById(launchId);
+  if (!aborted) {
+    return res.status(400).json({
+      error: 'Launch not aborted',
+    })
+  }
   // if launch does exist
   return res.status(200).json({
-    aborted
+    ok: true,
   });
 }
 
