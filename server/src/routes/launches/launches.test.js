b2e8472d4cac9ee4e7518f7bb468e6939624fc32
Simone <startae14@gmail.com>
2022-03-07 23:40:47 +0100

    update the API tests adding the version

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -16,7 +16,7 @@ describe('Launches API', () => {
   describe('Test GET /launches', () => {
     test('It should respond with 200 success', async () => {
       await request(app)
-        .get('/launches')
+        .get('/v1/launches')
         .expect('Content-Type', /json/)
         .expect(200);
     });
@@ -45,7 +45,7 @@ describe('Launches API', () => {
 
     test('It should respond with 201 created', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(completeLaunchData)
         .expect('Content-Type', /json/)
         .expect(201);
@@ -58,7 +58,7 @@ describe('Launches API', () => {
     });
     test('It should catch missing required properties', async () => {
       const response = await request(app)
-      .post('/launches')
+      .post('/v1/launches')
       .send(launchDataWithoutDate)
       .expect('Content-Type', /json/)
       .expect(400);
@@ -69,7 +69,7 @@ describe('Launches API', () => {
     });
     test('It should catch invalid dates', async () => {
       const response = await request(app)
-      .post('/launches')
+      .post('/v1/launches')
       .send(launchDataWithInvalideDate)
       .expect('Content-Type', /json/)
       .expect(400);
