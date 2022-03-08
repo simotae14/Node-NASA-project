86d2eecd68593c0ba4831f2636b6e8c8de6d8d92
Simone <startae14@gmail.com>
2022-03-08 14:48:31 +0100

    check if the first SapceX launch is already in the DB before call the API

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -21,7 +21,7 @@ saveLaunch(launch);
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
-async function loadLaunchData() {
+async function populateLaunches() {
   console.log('Downloading launch data...');
   const response = await axios.post(SPACEX_API_URL, {
     query: {},
@@ -62,15 +62,32 @@ async function loadLaunchData() {
     };
 
     console.log(`${launch.flightNumber} ${launch.mission}`);
+    // TODO: populate launches collection...
 
   }
 }
 
+async function loadLaunchData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: 'Falcon 1',
+    mission: 'FalconSat',
+  });
+  if (firstLaunch) {
+    console.log('Launch data already loaded');
+  } else {
+    await populateLaunches();
+  }
+}
+
+async function findLaunch(filter) {
+  return await launches.findOne(filter);
+}
+
 async function existsLaunchWithId(launchId) {
-  return await launches
-    .findOne({
-      flightNumber: launchId,
-    });
+  return await findLaunch({
+    flightNumber: launchId,
+  });
 }
 
 async function getLatestFlightNumber() {
