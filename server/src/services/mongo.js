7ea20fdb2c70fbe15e518c181fbd1bcec5efe46b
Simone <startae14@gmail.com>
2022-03-08 17:22:54 +0100

    use dotenv to hide the secrets, the API keys and other stuff from the source code

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:67OPi4F3xBirPFEl@nasacluster.raqb8.mongodb.net/nasa?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
