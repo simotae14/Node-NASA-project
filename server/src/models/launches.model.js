0335f73059c1dca09f31f9cf46cf3c17d8eb620f
Simone <startae14@gmail.com>
2022-03-08 16:15:17 +0100

    implement Pagination in our API endpoints

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -106,12 +106,14 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
+async function getAllLaunches(skip, limit) {
   return await launches
     .find({}, {
       '_id': 0,
       '__v': 0,
-    });
+    })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
