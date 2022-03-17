3204271d41d059d5e0f7c143d6882289b5c1d1b0
Simone <startae14@gmail.com>
2022-03-17 16:16:46 +0100

    update code
    Co-authored-by: startae14@hotmail.it <startae14@hotmail.it>

--- server/src/server.js
+++ server/src/server.js
@@ -3,11 +3,9 @@ const http = require('http');
 require('dotenv').config();
 
 const app = require('./app');
-
 const {
   mongoConnect
 } = require('./services/mongo');
-
 const { loadPlanetsData } = require('./models/planets.model');
 const { loadLaunchData } = require('./models/launches.model');
 
